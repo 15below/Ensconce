08bda71de8b72f40345fe91f57e02106cbbb8696
MJN <michael.newton@15below.com>
2012-07-03 13:11:13 +0100

    Fix adding project files, add deployHelp.ps1

---
+++ deployHelp.ps1
@@ -0,0 +1,14 @@
+﻿$deployToolsDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value.MyCommand.Path)
+
+if (Test-Path variable:\OctopusParameters)
+{
+    foreach($kp in $OctopusParameters.GetEnumerator())
+    {
+        Set-Content ("env:\" + $kp.Key) ($kp.Value) -Force
+    }
+}
+
+if(!(Test-Path "env:\ConfigOnly"))
+{
+    Set-Content "env:\ConfigOnly" $false
+}
