c091a88c239bd12827cbace8ab8dfcf24beb80ce
michael-steele-15below <michael.steele@15below.com>
2012-07-13 13:00:15 +0100

    Rebuilt roundhouse with AnyCPU. Corrected temporary restore path

--- 15below.Deployment.Database/DatabaseRestoreOptions.cs
+++ 15below.Deployment.Database/DatabaseRestoreOptions.cs
@@ -22,7 +22,7 @@ namespace FifteenBelow.Deployment
                 string database = Path.GetFileNameWithoutExtension(restorePath);
                 migrateSettings.Set(x => x.RestoreFromPath = restorePath)
                     .Set(x => x.Restore = !string.IsNullOrWhiteSpace(restorePath))
-                    .Set(x => x.RestoreCustomOptions = string.Format(", MOVE '{0}' TO '{1}{0}.mdf', MOVE '{0}_log' TO '{1}{0}_log.LDF'", database, @"c:\Temp"));
+                    .Set(x => x.RestoreCustomOptions = string.Format(@", MOVE '{0}' TO '{1}{0}.mdf', MOVE '{0}_log' TO '{1}\{0}_log.LDF'", database, @"c:\Temp"));
             }
         }
     }
