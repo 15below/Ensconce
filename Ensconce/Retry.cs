02aa326f2eab961835bd25711d48380fb7951a40
Yewridge <bruce.keen@15below.com>
2014-05-16 07:33:41 +0100

    Add a retry handler and use within the CopyDirectory function

---
+++ Ensconce/Retry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ensconce
+{
+    /// <summary>
+    /// Retry logic applied from this Stack Overflow thread
+    /// http://stackoverflow.com/questions/1563191/c-sharp-cleanest-way-to-write-retry-logic
+    /// </summary>
+    public static class Retry
+    {
+        public static void Do(Action action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            Do<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, retryCount);
+        }
+        public static T Do<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine("Something went wrong, but we're going to try again. ;(");
+                    Console.Out.WriteLine(ex.Message);
+                    exceptions.Add(ex);
+                    Thread.Sleep(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+}
