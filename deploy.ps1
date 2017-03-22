d62cb93237e133f107c845eb697d4d4363d8b52c
Chris Blyth <chris.blyth@15below.com>
2017-03-22 10:53:41 +0000

    Update deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,14 @@
 $scriptDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value.MyCommand.Path)
 
-Write-Host "Deploying Tools to $DeployToolsDir"
-
-if(!(Test-Path $DeployToolsDir))
+if(Test-Path $DeployPath)
 {
-    New-Item $DeployToolsDir -Type container
+    Write-Host "Removing Deploy Tools Directory Found at $DeployPath"
+    Remove-Item $DeployPath -Force -Recurse
+    Start-Sleep -s 2
 }
 
-Copy-Item "$scriptDir\Content\*" $DeployToolsDir -Force -Recurse
+Write-Host "Creating Deploy Tools Directory at $DeployPath"
+New-Item $DeployPath -Type container
+
+Write-Host "Deploying Tools to $DeployPath"
+Copy-Item "$scriptDir\Content\*" $DeployPath -Force -Recurse
