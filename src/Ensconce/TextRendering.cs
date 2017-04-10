56f4ed46274e59205c4ab156a3336593f6083774
Chris Blyth <chris.blyth@15below.com>
2017-04-10 15:28:03 +0100

    Remove dependency to render the fixed path
    If the file already exists without rendering it, then just create from that path directly

--- src/Ensconce/TextRendering.cs
+++ src/Ensconce/TextRendering.cs
@@ -8,7 +8,7 @@ namespace Ensconce
     internal static class TextRendering
     {
         private static readonly Lazy<TagDictionary> LazyTags = new Lazy<TagDictionary>(() => Retry.Do(BuildTagDictionary, TimeSpan.FromSeconds(5)));
-        public static IDictionary<string, object> TagDictionary { get { return LazyTags.Value; } }
+        public static IDictionary<string, object> TagDictionary => LazyTags.Value;
 
         internal static string Render(this string s)
         {
@@ -18,23 +18,46 @@ namespace Ensconce
         private static TagDictionary BuildTagDictionary()
         {
             var instanceName = Environment.GetEnvironmentVariable("InstanceName");
+            var fixedPath = Arguments.FixedPath;
+
+            //If file doesn't exist, try rendering the path as a template string
+            if (!File.Exists(fixedPath))
+            {
+                //Create a tag dictionary from environments to render
+                var tags = BuildTagDictionary(instanceName);
+                fixedPath = fixedPath.RenderTemplate(tags);
+                //build from that path, giving environment dictonary as a fallback
+                return BuildTagDictionary(instanceName, fixedPath, tags);
+            }
+
+            //Build using fixed path, do not provide a fall back
+            return BuildTagDictionary(instanceName, fixedPath, null);
+        }
+
+        private static TagDictionary BuildTagDictionary(string instanceName)
+        {
             Logging.Log("Building Tag Dictionary {0}", instanceName);
             var tags = new TagDictionary(instanceName);
             Logging.Log("Built Tag Dictionary {0}", instanceName);
+            return tags;
+        }
 
-            var renderedFixedPath = Arguments.FixedPath.RenderTemplate(tags);
+        private static TagDictionary BuildTagDictionary(string instanceName, string fixedPath, TagDictionary fallbackDictionary)
+        {
+            TagDictionary tags;
 
-            if (File.Exists(renderedFixedPath))
+            if (File.Exists(fixedPath))
             {
-                Logging.Log("Loading xml config from file {0}", Path.GetFullPath(renderedFixedPath));
-                var configXml = Retry.Do(() => File.ReadAllText(renderedFixedPath), TimeSpan.FromSeconds(5));
+                Logging.Log("Loading xml config from file {0}", Path.GetFullPath(fixedPath));
+                var configXml = Retry.Do(() => File.ReadAllText(fixedPath), TimeSpan.FromSeconds(5));
                 Logging.Log("Re-Building Tag Dictionary (Using Config File)");
                 tags = new TagDictionary(instanceName, configXml);
                 Logging.Log("Built Tag Dictionary (Using Config File)");
             }
             else
             {
-                Logging.Log("No structure file found at: {0}", Path.GetFullPath(renderedFixedPath));
+                Logging.Log("No structure file found at: {0}", Path.GetFullPath(fixedPath));
+                tags = fallbackDictionary ?? BuildTagDictionary();
             }
 
             return tags;
