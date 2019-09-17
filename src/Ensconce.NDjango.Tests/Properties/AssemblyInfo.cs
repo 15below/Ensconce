da7878faf384081c5b5c682489a7f3b87886c15d
Chris Blyth <BlythMeister@gmail.com>
2019-09-17 08:50:23 +0100

    Version Bump

--- src/Ensconce.NDjango.Tests/Properties/AssemblyInfo.cs
+++ src/Ensconce.NDjango.Tests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.1-local")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0-local")]
 
