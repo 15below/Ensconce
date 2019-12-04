b164f1e8675e8abb987de32ade7c8538d7ae2fab
Chris Blyth <blythmeister@gmail.com>
2019-12-04 15:29:16 +0000

    Add 7zip extraction support

--- src/Scripts/compressionHelper.ps1
+++ src/Scripts/compressionHelper.ps1
@@ -6,4 +6,25 @@ Function CreateZip([string]$sourcePath, [string]$destinationFile)
 	Compress-Archive -Path $sourcePath -DestinationPath $destinationFile
 }
 
+Function Get7zip() {
+  if ((Test-Path 'C:\Program Files\7-zip\7z.exe') -eq $true) {
+    return ('C:\"Program Files"\7-zip\7z.exe')
+  }
+  elseif ((Test-Path 'C:\Program Files (x86)\7-zip\7z.exe') -eq $true) {
+    return ('C:\"Program Files (x86)"\7-zip\7z.exe')
+  }
+}
+
+Function Extract7z([string]$sourceFile, [string]$destinationFolder)
+{
+	$7Zip = Get7zip
+	if ($7Zip -eq $null) {
+		throw 'Could not find 7-zip location'
+	}
+
+	Write-Host "Extracting '$sourceFile' into '$destinationFolder'"
+
+	Invoke-Expression ("$7Zip x $sourceFile -y -o$destinationFolder")
+}
+
 Write-Host "Ensconce - CompressionHelper Loaded"
