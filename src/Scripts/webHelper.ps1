4b43b34e4a158058c4abaa130fcf57b2c02436cb
Chris Blyth <blythmeister@gmail.com>
2019-12-04 15:35:07 +0000

    Add download string function

--- src/Scripts/webHelper.ps1
+++ src/Scripts/webHelper.ps1
@@ -3,6 +3,8 @@ Write-Host "Ensconce - WebHelper Loading"
 Function UploadFileAndGetStringResponse([string]$url, [string]$file)
 {
 	Write-Host "Uploading file '$file' to $url"
+	$webClient = New-Object System.Net.WebClient
+	$webClient.UseDefaultCredentials = $true
 	$responseBytes = $webClient.UploadFile($url, $file)
 	$response = [System.Text.Encoding]::ASCII.GetString($responseBytes)
 	Write-Host "Response: $response"
@@ -27,6 +29,9 @@ Function UploadValuesAndGetStringResponse([string]$url, [System.Collections.Spec
 		$value = $values[$key]
 		Write-Host "$key : $value"
 	}
+
+	$webClient = New-Object System.Net.WebClient
+	$webClient.UseDefaultCredentials = $true
 	$responseBytes = $webClient.UploadValues($url, $values)
 	$response = [System.Text.Encoding]::ASCII.GetString($responseBytes)
 	Write-Host "Response: $response"
@@ -36,7 +41,19 @@ Function UploadValuesAndGetStringResponse([string]$url, [System.Collections.Spec
 Function DownloadFile([string]$url, [string]$destinationPath)
 {
 	Write-Host "Downloading $url to $destinationPath"
-	Invoke-WebRequest -Uri $url -OutFile $destinationPath
+	$webClient = New-Object System.Net.WebClient
+	$webClient.UseDefaultCredentials = $true
+	$webClient.DownloadFile($url, $destinationPath)
+}
+
+Function DownloadString([string]$url)
+{
+	Write-Host "Downloading $url"
+	$webClient = New-Object System.Net.WebClient
+	$webClient.UseDefaultCredentials = $true
+	$response = $webClient.DownloadString($url)
+	Write-Host "Response: $response"
+	$response
 }
 
 Write-Host "Ensconce - WebHelper Loaded"
