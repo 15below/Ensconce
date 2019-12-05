b047ccbfffa781b0cbeda2e7be4fd7e6842f652a
Chris Blyth <blythmeister@gmail.com>
2019-12-05 09:42:10 +0000

    Tweak backupHelper

--- src/Scripts/backupHelper.ps1
+++ src/Scripts/backupHelper.ps1
@@ -6,20 +6,21 @@ if($deployHelpLoaded -eq $null)
 
 Write-Host "Ensconce - BackupHelper Loading"
 
-Function CleanBackupsWithDate([string]$backupDir, [int]$daysToKeep, [DateTime]$backupDate)
+Function CleanBackups([string]$backupDir, [int]$daysToKeep)
 {
-	$backupPurgedate = (get-date).AddDays($daysToKeep * -1)
+	if($daysToKeep > 0)
+	{
+		$backupPurgedate = (get-date).AddDays($daysToKeep * -1)
+	}
+	else
+	{
+		$backupPurgedate = (get-date).AddDays($daysToKeep)
+	}
 
 	if (@(Get-ChildItem $backupDir -Recurse | Where-Object { (-Not ($_.PSIsContainer)) }).Count -gt 0) {
 		Write-Host "Remove backups in $backupDir older than $daysToKeep days"
 		Get-ChildItem  -Path $backupDir -Recurse -Force | Where-Object { $_.CreationTime -lt $backupPurgedate -and (-Not ($_.PSIsContainer)) } | Remove-Item -Force
 	}
-}
-
-Function CleanBackups([string]$backupDir, [int]$daysToKeep)
-{
-	$backupDate = Get-Date -Format yyyy-MM-dd_HH-mm-ss
-	CleanBackupsWithDate $backupDir $backupDate
 }
 
 Function CreateDatedBackup([string]$destFolder, [string]$baseName, [string[]]$sources)
@@ -39,8 +40,7 @@ Function CreateDatedBackup([string]$destFolder, [string]$baseName, [string[]]$so
 
 Function Create7DayRollingDatedBackup([string]$destFolder, [string]$baseName, [string[]]$sources)
 {
-	$backupDate = Get-Date -Format yyyy-MM-dd_HH-mm-ss
-	CleanBackupsWithDate $destFolder 7 $backupDate
+	CleanBackupsWithDate $destFolder 7
 	CreateDatedBackup $destFolder $baseName $sources
 }
 
