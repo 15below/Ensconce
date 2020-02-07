89e7d4b0961f100dc16c10f66c4c8cdf110e7a7d
Chris Blyth <blythmeister@gmail.com>
2020-02-07 17:43:41 +0000

    Add rabbitHelper

---
+++ docs/powershell/rabbit-helper/index.md
@@ -0,0 +1,18 @@
+---
+title: rabbitHelper.ps1
+description: Details about the rabbitHelper.ps1 helper script
+---
+
+# rabbitHelper.ps1
+
+## Overview
+
+The `rabbitHelper.ps1` script has functionality to interact with a RabbitMQ server instance
+
+## Functions
+
+* CreateRabbitVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$vHost)
+* CreateRabbitUser([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$password)
+* AddUserToVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$vHost)
+* ValidateUserAccess([string]$serverAddress, [string]$user, [string]$password, [string]$vHost)
+* CreateRabbitUserAndVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$password, [string]$vHost)
---
+++ src/Scripts/rabbitHelper.ps1
@@ -0,0 +1,95 @@
+Write-Host "Ensconce - RabbitHelper Loading"
+
+function CreateRabbitVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$vHost)
+{
+	$deployPasswordSecure = ConvertTo-SecureString $deployPassword -AsPlainText -Force
+	$deployCreds = New-Object System.Management.Automation.PSCredential ($deployUser, $deployPasswordSecure)
+	$url = "http://${serverAddress}:15672/api"
+
+	if (Invoke-RestMethod -Uri "$url/vhosts" -Credential $deployCreds | where {$_.name -eq $vHost}) 
+	{
+		Write-Host "The virtual host $vHost already exists."
+	}
+	else 
+	{
+		Write-Host "Creating virtual host $vHost."
+		Invoke-RestMethod -Uri "$url/vhosts/$vHost" -Credential $deployCreds -DisableKeepAlive -Method Put -ContentType "application/json; charset=utf-8"
+	}
+}
+
+function CreateRabbitUser([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$password)
+{
+	$deployPasswordSecure = ConvertTo-SecureString $deployPassword -AsPlainText -Force
+	$deployCreds = New-Object System.Management.Automation.PSCredential ($deployUser, $deployPasswordSecure)
+	$url = "http://${serverAddress}:15672/api"
+	
+	if (Invoke-RestMethod -Uri "$url/users" -Credential $deployCreds | where {$_.name -eq $user}) 
+	{
+		Write-Host "Updating user $user."
+		$updatedUser = "{""password"":""$password"",""tags"":""management""}"
+		$utf = [System.Text.Encoding]::Utf8.GetBytes($updatedUser)
+		Invoke-RestMethod -Uri "$url/users/$user" -Credential $deployCreds -DisableKeepAlive -Method Put -Body $utf -ContentType "application/json; charset=utf-8"
+	}
+	else 
+	{
+		Write-Host "Creating user $user."
+		$newUser = "{""password"":""$password"",""tags"":""management""}"
+		$utf = [System.Text.Encoding]::Utf8.GetBytes($newUser)
+		Invoke-RestMethod -Uri "$url/users/$user" -Credential $deployCreds -DisableKeepAlive -Method Put -Body $utf -ContentType "application/json; charset=utf-8"
+	}
+}
+
+function AddUserToVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$vHost)
+{
+	$deployPasswordSecure = ConvertTo-SecureString $deployPassword -AsPlainText -Force
+	$deployCreds = New-Object System.Management.Automation.PSCredential ($deployUser, $deployPasswordSecure)
+	$url = "http://${serverAddress}:15672/api"
+	
+	if (Invoke-RestMethod -Uri "$url/users" -Credential $deployCreds | where {$_.name -eq $user}) {
+		if (Invoke-RestMethod -Uri "$url/vhosts" -Credential $deployCreds | where {$_.name -eq $vHost}) {				
+			Write-Host "Giving $user access to $vHost."
+			$newPermission = "{""configure"":"".*"",""write"":"".*"",""read"":"".*""}"
+			Invoke-RestMethod -Uri "$url/permissions/$vHost/$user" -Credential $deployCreds -DisableKeepAlive -Method Put -Body $newPermission -ContentType "application/json; charset=utf-8"
+		}
+		else 
+		{
+			Write-Error "The virtual host $vHost does not exist."
+			Exit 123
+		}
+	}
+	else
+	{
+		Write-Error "The user $user does not exist."
+		Exit 124
+	}
+}
+
+function ValidateUserAccess([string]$serverAddress, [string]$user, [string]$password, [string]$vHost)
+{
+	$passwordSecure = ConvertTo-SecureString $password -AsPlainText -Force
+	$creds = New-Object System.Management.Automation.PSCredential ($user, $passwordSecure)
+	$url = "http://${serverAddress}:15672/api"
+	
+	try {
+		Write-Host "Validating user $user has been set up correctly."
+		$queues = Invoke-RestMethod -uri "$url/queues/$vHost" -Credential $creds
+	}
+	catch [Exception] {
+		Write-Error $_.Exception 
+		Exit 125
+	}
+}
+
+function CreateRabbitUserAndVHost([string]$deployUser, [string]$deployPassword, [string]$serverAddress, [string]$user, [string]$password, [string]$vHost)
+{
+	CreateRabbitVHost $deployUser $deployPassword $vHost
+	
+	CreateRabbitUser $deployUser $deployPassword $user $password
+	
+	AddUserToVHost $deployUser $deployPassword $user $vHost
+	
+	ValidateUserAccess $user $password $vHost
+}
+
+
+Write-Host "Ensconce - RabbitHelper Loaded"
