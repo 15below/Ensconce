5d88e6b26f3b420f1af6fdf6e9875f6f10759ced
Chris Blyth <blythmeister@gmail.com>
2020-03-17 09:50:56 +0000

    Deployment script bug fix

--- src/Deploy/deploy.ps1
+++ src/Deploy/deploy.ps1
@@ -3,16 +3,16 @@ $scriptDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value.MyCommand.Pa
 if(Test-Path $DeployPath)
 {
     Write-Host "Removing Deploy Tools Directory Found at $DeployPath"
-    Remove-Item $DeployPath -Force -Recurse
+    Remove-Item $DeployPath -Force -Recurse | Out-Null
     Start-Sleep -s 2
 }
 
 Write-Host "Creating Deploy Tools Directory at $DeployPath"
-New-Item $DeployPath -Type container
+New-Item -Path $DeployPath -Type container -Force | Out-Null
 
-if($includeK8s -eq "True")
+if($IncludeK8s -eq "True")
 {
-	Write-Host "Ensconce deployed in Kubernetes mode as 'includeK8s' set to 'True'"
+	Write-Host "Ensconce deployed in Kubernetes mode as 'IncludeK8s' set to 'True'"
 	Remove-Item "$scriptDir\Content\backupHelper.ps1" -Force
 	Remove-Item "$scriptDir\Content\createWebSite.ps1" -Force
 	Remove-Item "$scriptDir\Content\dnsHelper.ps1" -Force
@@ -20,7 +20,7 @@ if($includeK8s -eq "True")
 }
 else
 {
-	Write-Host "Ensconce deployed in application deployment mode as 'includeK8s' NOT set to 'True'"
+	Write-Host "Ensconce deployed in application deployment mode as 'IncludeK8s' NOT set to 'True'"
 	Remove-Item "$scriptDir\Content\Tools\KubeCtl" -Force -Recurse
 	Remove-Item "$scriptDir\Content\kubernetesHelper.ps1" -Force
 }
@@ -36,9 +36,9 @@ Get-ChildItem -Path $scriptDir\Content\Tools | ForEach-Object {
 	$toolName = $_.Name
 	$toolFullName = $_.FullName
 	Write-Host "Deploying tool $toolName to $DeployPath\Tools"
-	New-Item -Path "$DeployPath\Tools" -Name $toolName -ItemType "Directory" -Force | Out-Null
+	New-Item -Path $DeployPath\Tools -Name $toolName -Type container -Force | Out-Null
 	Copy-Item -Path $toolFullName -Destination $DeployPath\Tools -Force -Recurse
 }
 
 Write-Host "Create releaseVersion.txt"
-New-Item -Path $DeployPath -Name "releaseVersion.txt" -ItemType "file" -Value $VersionNumber
+New-Item -Path $DeployPath -Name "releaseVersion.txt" -ItemType "file" -Value $VersionNumber | Out-Null
