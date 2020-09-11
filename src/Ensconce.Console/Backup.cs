73a4cded76903f40dcac042f1eed30df3f6977bf
Chris Blyth <blythmeister@gmail.com>
2020-09-11 12:06:27 +0100

    Fix backup to make zip readable in windows & handle 2 folders with same name

--- src/Ensconce.Console/Backup.cs
+++ src/Ensconce.Console/Backup.cs
@@ -28,43 +28,41 @@ namespace Ensconce.Console
                 backupDestination.Directory.Create();
             }
 
-            if (backupSources.Count == 1)
+            using (var zipFile = ZipFile.Open(backupDestination.FullName, ZipArchiveMode.Create))
             {
-                Logging.Log($"Adding {backupSources[0]} to {backupDestination.FullName}");
-                ZipFile.CreateFromDirectory(backupSources[0].FullName, backupDestination.FullName, CompressionLevel.Optimal, true);
-                Logging.Log($"Added {backupSources[0]} to {backupDestination.FullName}");
-            }
-            else
-            {
-                using (var zipFile = ZipFile.Open(backupDestination.FullName, ZipArchiveMode.Create))
+                foreach (var backupSource in backupSources)
                 {
-                    foreach (var backupSource in backupSources)
+                    Logging.Log($"Adding {backupSource} to {backupDestination.FullName}");
+                    var fileCount = 0;
+                    var emptyDirCount = 0;
+                    foreach (var fileSystemInfo in backupSource.EnumerateFileSystemInfos("*", SearchOption.AllDirectories))
                     {
-                        Logging.Log($"Adding {backupSource} to {backupDestination.FullName}");
+                        var entryName = fileSystemInfo.FullName.Replace(":\\", "_\\");
 
-                        var files = backupSource.GetFiles("**", SearchOption.AllDirectories);
-                        if (files.Length == 0)
+                        switch (fileSystemInfo)
                         {
-                            zipFile.CreateEntry(backupSource.Name);
-                        }
-                        else
-                        {
-                            foreach (var file in files)
-                            {
-                                if (backupSource.Parent != null)
-                                {
-                                    zipFile.CreateEntryFromFile(file.FullName, file.FullName.Replace(backupSource.Parent.FullName, ""), CompressionLevel.Optimal);
-                                }
-                                else
-                                {
-                                    zipFile.CreateEntryFromFile(file.FullName, file.FullName.Replace(backupSource.FullName, ""), CompressionLevel.Optimal);
-                                }
-                            }
-                        }
+                            case FileInfo file:
+                                fileCount++;
+                                zipFile.CreateEntryFromFile(file.FullName, entryName, CompressionLevel.Fastest);
+                                break;
 
-                        Logging.Log($"Added {backupSource} to {backupDestination.FullName}");
+                            case DirectoryInfo possiblyEmptyDir when IsDirEmpty(possiblyEmptyDir):
+                                emptyDirCount++;
+                                zipFile.CreateEntry($"{entryName}\\", CompressionLevel.Fastest);
+                                break;
+                        }
                     }
+
+                    Logging.Log($"Added {backupSource} to {backupDestination.FullName} ({fileCount} files / {emptyDirCount} empty directories)");
                 }
+            }
+        }
+
+        private static bool IsDirEmpty(DirectoryInfo possiblyEmptyDir)
+        {
+            using (var enumerator = possiblyEmptyDir.EnumerateFileSystemInfos("*", SearchOption.AllDirectories).GetEnumerator())
+            {
+                return !enumerator.MoveNext();
             }
         }
     }
