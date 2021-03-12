e0a3df8344a1e51d18afc6a6844491d561d3198b
Chris Blyth <blythmeister@gmail.com>
2021-03-12 15:45:34 +0000

    Handle update based on a path object as well as the directory/file

--- src/Ensconce.Cake/EnsconceFileUpdateExtensions.cs
+++ src/Ensconce.Cake/EnsconceFileUpdateExtensions.cs
@@ -8,19 +8,34 @@ namespace Ensconce.Cake
     {
         public static void TextSubstitute(this IFile file, FilePath fixedStructureFile)
         {
-            var tagDictionary = fixedStructureFile == null ? TagDictionaryBuilder.Build(string.Empty) : TagDictionaryBuilder.Build(fixedStructureFile.FullPath);
-            Update.ProcessFiles.UpdateFile(new FileInfo(file.Path.FullPath), tagDictionary);
+            file.Path.TextSubstitute(fixedStructureFile);
         }
 
         public static void TextSubstitute(this IDirectory directory, FilePath fixedStructureFile)
         {
-            directory.TextSubstitute("*.*", fixedStructureFile);
+            directory.Path.TextSubstitute(fixedStructureFile);
         }
 
         public static void TextSubstitute(this IDirectory directory, string filter, FilePath fixedStructureFile)
         {
+            directory.Path.TextSubstitute(filter, fixedStructureFile);
+        }
+
+        public static void TextSubstitute(this FilePath file, FilePath fixedStructureFile)
+        {
             var tagDictionary = fixedStructureFile == null ? TagDictionaryBuilder.Build(string.Empty) : TagDictionaryBuilder.Build(fixedStructureFile.FullPath);
-            Update.ProcessFiles.UpdateFiles(directory.Path.FullPath, filter, tagDictionary);
+            Update.ProcessFiles.UpdateFile(new FileInfo(file.FullPath), tagDictionary);
+        }
+
+        public static void TextSubstitute(this DirectoryPath directory, FilePath fixedStructureFile)
+        {
+            directory.TextSubstitute("*.*", fixedStructureFile);
+        }
+
+        public static void TextSubstitute(this DirectoryPath directory, string filter, FilePath fixedStructureFile)
+        {
+            var tagDictionary = fixedStructureFile == null ? TagDictionaryBuilder.Build(string.Empty) : TagDictionaryBuilder.Build(fixedStructureFile.FullPath);
+            Update.ProcessFiles.UpdateFiles(directory.FullPath, filter, tagDictionary);
         }
     }
 }
