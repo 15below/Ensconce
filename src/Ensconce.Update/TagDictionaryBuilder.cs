8fdacb8dfdb8904f7207c9ec43367a62f0b6dacc
Chris Blyth <blythmeister@gmail.com>
2021-03-29 12:58:21 +0100

    Update logging

--- src/Ensconce.Update/TagDictionaryBuilder.cs
+++ src/Ensconce.Update/TagDictionaryBuilder.cs
@@ -26,9 +26,9 @@ namespace Ensconce.Update
         private static TagDictionary BuildTagDictionary()
         {
             var instanceName = Environment.GetEnvironmentVariable("InstanceName");
-            Logging.Log("Building Tag Dictionary {0}", instanceName);
+            Logging.Log("Building tag dictionary with instance '{0}' from environment", instanceName);
             var tags = TagDictionary.FromIdentifier(instanceName);
-            Logging.Log("Built Tag Dictionary {0}", instanceName);
+            Logging.Log("Built tag dictionary with instance '{0}' from environment", instanceName);
             return tags;
         }
 
@@ -41,15 +41,15 @@ namespace Ensconce.Update
             if (fileExists)
             {
                 var instanceName = Environment.GetEnvironmentVariable("InstanceName");
-                Logging.Log("Loading xml config from file {0}", path);
+                Logging.Log("Loading xml config from file '{0}'", path);
                 var configXml = Retry.Do(() => File.ReadAllText(path), TimeSpan.FromSeconds(5));
-                Logging.Log("Re-Building Tag Dictionary (Using config file '{0}')", path);
+                Logging.Log("Building tag dictionary with instance '{0}' (using config file '{1}')", instanceName, path);
                 tags = TagDictionary.FromXml(instanceName, configXml);
-                Logging.Log("Re-Built Tag Dictionary (Using config file '{0}')", path);
+                Logging.Log("Built tag dictionary with instance '{0}' (using config file '{1}')", instanceName, path);
             }
             else
             {
-                Logging.Log("WARNING: No structure file found at: '{0}' using base dictionary", path);
+                Logging.Log("WARNING: No structure file found at: '{0}'", path);
                 tags = fallbackDictionary.Value;
             }
 
