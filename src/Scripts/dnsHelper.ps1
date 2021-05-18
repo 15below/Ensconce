2b227d18cd3a7cc63b7dca4812ac6c3a5d884e75
Chris Blyth <blythmeister@gmail.com>
2021-05-18 16:54:59 +0100

    Update to DNS Helper to handle different TTL values
    Also means updates to not reset the TTL to 3600

--- src/Scripts/dnsHelper.ps1
+++ src/Scripts/dnsHelper.ps1
@@ -1,68 +1,73 @@
 Write-Host "Ensconce - dnsHelper Loading"
 
-function GetAllSubValues ([string]$dnsServer, [string]$domain, [string]$lookupName)
+function GetDnsRecords ([string]$dnsServer, [string]$domain, [string]$lookupName)
 {
     $result = dnscmd $dnsServer /EnumRecords $domain $lookupName
 
-    $keys = New-Object Collections.Generic.List[string]
+    $keys = New-Object Collections.Generic.List[pscustomobject]
     foreach ($item in $result)
     {
-        if ($item.Contains("3600 CNAME") -or ($item.Contains("3600 A")))
+        $match = [regex]::Match($item, "^(?<key>\S*)\s+(?<ttl>\d+)\s+(?<type>\S+)\s+(?<value>.+)$")
+        if($match.Success)
         {
-            $key = $item -replace "\s*3600.*", ""
+            $key = $match.Groups["key"].Value
+            $ttl = $match.Groups["ttl"].Value
+            $type = $match.Groups["type"].Value
+            $value = $match.Groups["value"].Value
+
             if($key -eq "@")
             {
-                $keys.Add("$lookupName".ToLower())
+                $record = $lookupName
             }
             else
             {
-                $keys.Add("$key.$lookupName".ToLower())
+                $record = "$key.$lookupName"
             }
+
+            if($value.EndsWith("."))
+            {
+                $value = $value.Substring(0,$value.Length-1)
+            }
+
+            $keys.Add([pscustomobject]@{Record=$record;Ttl=$ttl;Type=$type;Value=$value})
         }
+
     }
 
     $keys
 }
 
+function GetAllSubValues ([string]$dnsServer, [string]$domain, [string]$lookupName)
+{
+    GetDnsRecords $dnsServer $domain $lookupName | Select -ExpandProperty Record
+}
+
 function CheckName ([string]$dnsServer, [string]$domain, [string]$lookupName)
 {
-    $result = dnscmd $dnsServer /EnumRecords $domain $lookupName /node
-    $outcome = $False
-    foreach ($item in $result)
+    $dnsRecords = GetDnsRecords $dnsServer $domain $lookupName
+    $checkResult = ($dnsRecords | Where-Object {$_.Record -eq $lookupName} | measure).Count -eq 1
+
+    if($checkResult -eq $false)
     {
-        if ($item.Contains("3600 CNAME") -or ($item.Contains("3600 A")))
-        {
-            $outcome = $True
-        }
+        Write-Host "CheckName is false records found:"
+        Write-Host ($dnsRecords | Out-String)
     }
 
-    if($outcome -eq $false)
-    {
-        write-host "dnscmd: $result"
-    }
-
-    $outcome
+    $checkResult
 }
 
 function CheckCNameValue ([string]$dnsServer, [string]$domain, [string]$name, [string]$server)
 {
-    $result = dnscmd $dnsServer /EnumRecords $domain $name /node
+    $dnsRecords = GetDnsRecords $dnsServer $domain $name
+    $checkResult = ($dnsRecords | Where-Object {$_.Record -eq $name -and $_.Type -eq "CNAME" -and $_.Value -eq $server} | measure).Count -eq 1
 
-    $outcome = $False
-    foreach ($item in $result)
+    if($checkResult -eq $false)
     {
-        if ($item.Contains("3600 CNAME") -and ($item.ToLower().Contains($server.ToLower())))
-        {
-            $outcome = $True
-        }
+        Write-Host "CheckCNameValue is false records found:"
+        Write-Host ($dnsRecords | Out-String)
     }
 
-    if($outcome -eq $false)
-    {
-        write-host "dnscmd: $result"
-    }
-
-    $outcome
+    $checkResult
 }
 
 function DeleteCName ([string]$dnsServer, [string]$domain, [string]$name)
@@ -86,10 +91,10 @@ function DeleteCName ([string]$dnsServer, [string]$domain, [string]$name)
     $outcome
 }
 
-function CreateCName ([string]$dnsServer, [string]$domain, [string]$name, [string]$server)
+function CreateCName ([string]$dnsServer, [string]$domain, [string]$name, [string]$server, [string]$ttl="3600")
 {
-    write-host "Creating DNS CNAME record for $name.$domain pointing at $server"
-    $result = dnscmd $dnsServer /recordAdd $domain $name CNAME $server
+    write-host "Creating DNS CNAME record for $name.$domain pointing at $server with TTL $ttl"
+    $result = dnscmd $dnsServer /recordAdd $domain $name $ttl CNAME $server
     $outcome = $false
     foreach ($item in $result)
     {
@@ -109,9 +114,18 @@ function CreateCName ([string]$dnsServer, [string]$domain, [string]$name, [strin
 
 function UpdateCName ([string]$dnsServer, [string]$domain, [string]$name, [string]$server)
 {
-    DeleteCName $dnsServer $domain $name
-    DeleteARecord $dnsServer $domain $name
-    CreateCName $dnsServer $domain $name $server
+    $currentRecord = (GetDnsRecords $dnsServer $domain $name | Where-Object {$_.Record -eq $name})
+    $currentTtl = ($currentRecord | Select-Object -ExpandProperty Ttl -First 1)
+    $currentType = ($currentRecord | Select-Object -ExpandProperty Type -First 1)
+    if($currentType -eq "CNAME")
+    {
+        DeleteCName $dnsServer $domain $name
+    }
+    if($currentType -eq "A")
+    {
+        DeleteARecord $dnsServer $domain $name
+    }
+    CreateCName $dnsServer $domain $name $server $currentTtl
 }
 
 function CreateOrUpdateCName ([string]$dnsServer, [string]$domain, [string]$name, [string]$server, [bool]$warnOnUpdate = $false)
@@ -162,22 +176,16 @@ function CreateOrUpdateCName ([string]$dnsServer, [string]$domain, [string]$name
 
 function CheckARecordValue ([string]$dnsServer, [string]$domain, [string]$name, [string]$ipAddress)
 {
-    $result = dnscmd $dnsServer /EnumRecords $domain $name /node
-    $outcome = $False
-    foreach ($item in $result)
+    $dnsRecords = GetDnsRecords $dnsServer $domain $name
+    $checkResult = ($dnsRecords | Where-Object {$_.Record -eq $name -and $_.Type -eq "A" -and $_.Value -eq $ipAddress} | measure).Count -eq 1
+
+    if($checkResult -eq $false)
     {
-        if ($item.Contains("3600 A") -and ($item.Contains($ipAddress)))
-        {
-            $outcome = $True
-        }
+        Write-Host "CheckARecordValue is false records found:"
+        Write-Host ($dnsRecords | Out-String)
     }
 
-    if($outcome -eq $false)
-    {
-        write-host "dnscmd: $result"
-    }
-
-    $outcome
+    $checkResult
 }
 
 function DeleteARecord ([string]$dnsServer, [string]$domain, [string]$name)
@@ -201,9 +209,9 @@ function DeleteARecord ([string]$dnsServer, [string]$domain, [string]$name)
     $outcome
 }
 
-function CreateARecord ([string]$dnsServer, [string]$domain, [string]$name, [string]$ipAddress)
+function CreateARecord ([string]$dnsServer, [string]$domain, [string]$name, [string]$ipAddress, [string]$ttl="3600")
 {
-    write-host "Creating DNS A record for $name.$domain pointing at $ipAddress"
+    write-host "Creating DNS A record for $name.$domain pointing at $ipAddress with TTL $ttl"
     $result = dnscmd $dnsServer /recordAdd $domain $name A $ipAddress
     $outcome = $false
     foreach ($item in $result)
@@ -224,9 +232,18 @@ function CreateARecord ([string]$dnsServer, [string]$domain, [string]$name, [str
 
 function UpdateARecord ([string]$dnsServer, [string]$domain, [string]$name, [string]$ipAddress)
 {
-    DeleteCName $dnsServer $domain $name
-    DeleteARecord $dnsServer $domain $name
-    CreateARecord $dnsServer $domain $name $ipAddress
+    $currentRecord = (GetDnsRecords $dnsServer $domain $name | Where-Object {$_.Record -eq $name})
+    $currentTtl = ($currentRecord | Select-Object -ExpandProperty Ttl -First 1)
+    $currentType = ($currentRecord | Select-Object -ExpandProperty Type -First 1)
+    if($currentType -eq "CNAME")
+    {
+        DeleteCName $dnsServer $domain $name
+    }
+    if($currentType -eq "A")
+    {
+        DeleteARecord $dnsServer $domain $name
+    }
+    CreateARecord $dnsServer $domain $name $ipAddress $currentTtl
 }
 
 function CreateOrUpdateARecord ([string]$dnsServer, [string]$domain, [string]$name, [string]$ipAddress, [bool]$warnOnUpdate = $false)
